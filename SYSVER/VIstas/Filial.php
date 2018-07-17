c9f6daddf80237269588a3762c8ae7547de0ee74
Jesus <jordanchavez@upeu.edu.pe>
2018-07-17 16:23:59 -0500

    Ojala nos paguen :'v

--- SYSVER/VIstas/Filial.php
+++ SYSVER/VIstas/Filial.php
@@ -7,40 +7,40 @@
     <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
     <title>SYSVER | Brandux </title>
     <!-- Favicon-->
-    <link rel="icon" href="favicon.ico" type="image/x-icon">
+    <link rel="icon" href="../favicon.ico" type="image/x-icon">
 
     <!-- Google Fonts -->
     <link href="https://fonts.googleapis.com/css?family=Roboto:400,700&subset=latin,cyrillic-ext" rel="stylesheet" type="text/css">
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet" type="text/css">
 
     <!-- Bootstrap Core Css -->
-    <link href="plugins/bootstrap/css/bootstrap.css" rel="stylesheet">
+    <link href="../plugins/bootstrap/css/bootstrap.css" rel="stylesheet">
 
     <!-- Waves Effect Css -->
-    <link href="plugins/node-waves/waves.css" rel="stylesheet" />
+    <link href="../plugins/node-waves/waves.css" rel="stylesheet" />
 
     <!-- Animation Css -->
-    <link href="plugins/animate-css/animate.css" rel="stylesheet" />
+    <link href="../plugins/animate-css/animate.css" rel="stylesheet" />
 
     <!-- Morris Chart Css-->
-    <link href="plugins/morrisjs/morris.css" rel="stylesheet" />
+    <link href="../plugins/morrisjs/morris.css" rel="stylesheet" />
     
     
     <!-- JQuery DataTable Css -->
-    <link href="plugins/jquery-datatable/skin/bootstrap/css/dataTables.bootstrap.css" rel="stylesheet">
+    <link href="../plugins/jquery-datatable/skin/bootstrap/css/dataTables.bootstrap.css" rel="stylesheet">
 
     <!-- Animation Css -->
-    <link href="plugins/animate-css/animate.css" rel="stylesheet" />
+    <link href="../plugins/animate-css/animate.css" rel="stylesheet" />
 
     <!-- Sweetalert Css -->
-    <link href="plugins/sweetalert/sweetalert.css" rel="stylesheet" />
+    <link href="../plugins/sweetalert/sweetalert.css" rel="stylesheet" />
 
     
     <!-- Custom Css -->
-    <link href="css/style.css" rel="stylesheet">
+    <link href="../css/style.css" rel="stylesheet">
 
     <!-- AdminBSB Themes. You can choose a theme from css/themes instead of get all themes -->
-    <link href="css/themes/all-themes.css" rel="stylesheet" />
+    <link href="../css/themes/all-themes.css" rel="stylesheet" />
     
 </head>
 
@@ -101,7 +101,7 @@
             <!-- User Info -->
             <div class="user-info">
                 <div class="image">
-                    <img src="images/user.png" width="48" height="48" alt="User" />
+                    <img src="../images/user.png" width="48" height="48" alt="User" />
                 </div>
                 <div class="info-container">
                     <div class="name" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">John Doe</div>
@@ -127,19 +127,19 @@
                             <span>Trabajadores</span>
                         </a>
                     </li>
-                    <li  class="active">
+                    <li >
                         <a href="PROYECTOS.html">
                             <i class="material-icons">content_paste</i>
                             <span>Proyectos</span>
                         </a>
                     </li>
-                    <li>
+                    <li >
                         <a href="CATEGORIAS.html">
                             <i class="material-icons">attach_file</i>
                             <span>Categorias</span>
                         </a>
                     </li>
-                    <li>
+                    <li   class="active">
                         <a href="FILIA.html">
                             <i class="material-icons">account_balance</i>
                             <span>Filial</span>
@@ -170,7 +170,7 @@
                                     <div class="card">
                                         <div class="header">
                                             <h2>
-                                               Lista de Proyectos
+                                               Lista de Categorias de proyectos
                                             </h2>
                                             <ul class="header-dropdown m-r--5">
                                                 <li class="dropdown">
@@ -187,7 +187,7 @@
                                         </div>
                                         
                                         <div class="body">
-                                              <button type="button"  class="btn bg-blue btn-circle-lg waves-effect waves-circle waves-float">
+                                              <button type="button"  data-toggle="modal" data-target="#guardar"  class="btn bg-blue btn-circle-lg waves-effect waves-circle waves-float">
                                                     <i class="material-icons">add</i>
                                                 </button>
                                             <br><br>
@@ -195,25 +195,19 @@
                                                 <table class="table table-bordered table-striped table-hover dataTable js-exportable">
                                                     <thead>
                                                         <tr>
-                                                            <th>Cliente </th>
-                                                            <th>Proyecto</th>
-                                                            <th>Fecha de duracion</th>
-                                                            <th>Costo total</th>
-                                                            <th>Est de suelos</th>
-                                                            <th>Categoria</th>
-                                                            <th>Estado</th>
+                                                            <th>Filial </th>
+                                                            <th>Direccion  </th>
+                                                            <th>celular </th>
+                                                            <th>ruc  </th>
                                                             <th>OPIONES</th>
                                                         </tr>
                                                     </thead>
                                                     <tfoot>
                                                         <tr>
-                                                            <th>Cliente </th>
-                                                            <th>Proyecto</th>
-                                                            <th>Fecha de duracion</th>
-                                                            <th>Costo total</th>
-                                                            <th>Est de suelos</th>
-                                                            <th>Categoria</th>
-                                                            <th>Estado</th>
+                                                            <th>Filial </th>
+                                                            <th>Direccion  </th>
+                                                            <th>celular </th>
+                                                            <th>ruc  </th>
                                                             <th>OPIONES</th>
                                                         </tr>
                                                     </tfoot>
@@ -221,12 +215,9 @@
                                                           
                                                         <tr>
                                                             <td>Michael Bruce</td>
-                                                            <td>Javascript Developer</td>
-                                                            <td>Singapore</td>
-                                                            <td>29</td>
-                                                            <td>2011/06/27</td>
-                                                            <td>2011/06/27</td>
-                                                            <td>2011/06/27</td>
+                                                            <td>Michael Bruce</td>
+                                                            <td>Michael Bruce</td>
+                                                            <td>Michael Bruce</td>
                                                             <td>
                                                                 <button type="button"  data-toggle="modal" data-target="#edit"  class="btn btn-info btn-circle waves-effect waves-circle waves-float">
                                                                     <i class="material-icons">edit</i>
@@ -238,12 +229,9 @@
                                                         </tr>
                                                         <tr>
                                                             <td>Donna Snider</td>
-                                                            <td>Customer Support</td>
-                                                            <td>New York</td>
-                                                            <td>27</td>
-                                                            <td>2011/01/25</td>
-                                                            <td>$112,000</td>
-                                                            <td>2011/06/27</td>
+                                                            <td>Michael Bruce</td>
+                                                            <td>Michael Bruce</td>
+                                                            <td>Michael Bruce</td>
                                                             <td>
                                                                 <button type="button"  data-toggle="modal" data-target="#edit" class="btn btn-info btn-circle waves-effect waves-circle waves-float">
                                                                     <i class="material-icons">edit</i>
@@ -301,45 +289,85 @@
                         </div>
                     </div>
                 </div>
+            
+            <!-- Default Size -->
+                <div class="modal fade" id="guardar" tabindex="-1" role="dialog">
+                    <div class="modal-dialog" role="document">
+                        <div class="modal-content">
+                            <div class="modal-header">
+                                <h4 class="modal-title" id="defaultModalLabel">Actualizar Datos de proyectos</h4>
+                            </div>
+                            <div class="modal-body">
+                            <form>
+                                <div class="form-group">
+                                    <div class="form-line">
+                                        <input type="text" id="email_address" class="form-control" placeholder="Corre Electronico">
+                                    </div>
+                                </div>
+                                <div class="form-group">
+                                    <div class="form-line">
+                                        <input type="password" id="password" class="form-control" placeholder="Fechas fin">
+                                    </div>
+                                </div>
+                                <div class="form-group">
+                                    <div class="form-line">
+                                        <input type="text" id="email_address" class="form-control" placeholder="Sueldo">
+                                    </div>
+                                </div>
+                                <div class="form-group">
+                                    <div class="form-line">
+                                        <input type="text" id="email_address" class="form-control" placeholder="Sueldo">
+                                    </div>
+                                </div>
+                            </form>                                
+                            </div>
+                            <div class="modal-footer">
+                                <button type="button" class="btn btn-link waves-effect">Guardar Cambios</button>
+                                <button type="button" class="btn btn-link waves-effect" data-dismiss="modal">Cerrar</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            
         </div>
     </section>
 
     <!-- Jquery Core Js -->
-    <script src="plugins/jquery/jquery.min.js"></script>
+    <script src="../plugins/jquery/jquery.min.js"></script>
 
     <!-- Bootstrap Core Js -->
-    <script src="plugins/bootstrap/js/bootstrap.js"></script>
+    <script src="../plugins/bootstrap/js/bootstrap.js"></script>
 
     <!-- Select Plugin Js -->
-    <script src="plugins/bootstrap-select/js/bootstrap-select.js"></script>
+    <script src="../plugins/bootstrap-select/js/bootstrap-select.js"></script>
 
     <!-- Slimscroll Plugin Js -->
-    <script src="plugins/jquery-slimscroll/jquery.slimscroll.js"></script>
+    <script src="../plugins/jquery-slimscroll/jquery.slimscroll.js"></script>
 
     <!-- Waves Effect Plugin Js -->
-    <script src="plugins/node-waves/waves.js"></script>
+    <script src="../plugins/node-waves/waves.js"></script>
 
 <!-- ESTOS  SN IMPORTNTES-->
         
     <!-- Jquery DataTable Plugin Js -->
-    <script src="plugins/jquery-datatable/jquery.dataTables.js"></script>
-    <script src="plugins/jquery-datatable/skin/bootstrap/js/dataTables.bootstrap.js"></script>
-    <script src="plugins/jquery-datatable/extensions/export/dataTables.buttons.min.js"></script>
-    <script src="plugins/jquery-datatable/extensions/export/buttons.flash.min.js"></script>
-    <script src="plugins/jquery-datatable/extensions/export/jszip.min.js"></script>
-    <script src="plugins/jquery-datatable/extensions/export/pdfmake.min.js"></script>
-    <script src="plugins/jquery-datatable/extensions/export/vfs_fonts.js"></script>
-    <script src="plugins/jquery-datatable/extensions/export/buttons.html5.min.js"></script>
-    <script src="plugins/jquery-datatable/extensions/export/buttons.print.min.js"></script>
+    <script src="../plugins/jquery-datatable/jquery.dataTables.js"></script>
+    <script src="../plugins/jquery-datatable/skin/bootstrap/js/dataTables.bootstrap.js"></script>
+    <script src="../plugins/jquery-datatable/extensions/export/dataTables.buttons.min.js"></script>
+    <script src="../plugins/jquery-datatable/extensions/export/buttons.flash.min.js"></script>
+    <script src="../plugins/jquery-datatable/extensions/export/jszip.min.js"></script>
+    <script src="../plugins/jquery-datatable/extensions/export/pdfmake.min.js"></script>
+    <script src="../plugins/jquery-datatable/extensions/export/vfs_fonts.js"></script>
+    <script src="../plugins/jquery-datatable/extensions/export/buttons.html5.min.js"></script>
+    <script src="../plugins/jquery-datatable/extensions/export/buttons.print.min.js"></script>
 
     <!-- SweetAlert Plugin Js -->
-    <script src="plugins/sweetalert/sweetalert.min.js"></script>
+    <script src="../plugins/sweetalert/sweetalert.min.js"></script>
 
     <!-- Custom Js -->
-    <script src="js/admin.js"></script>
-    <script src="js/pages/tables/jquery-datatable.js"></script>
-    <script src="js/pages/ui/dialogs.js"></script>
-    <script src="js/demo.js"></script>
+    <script src="../js/admin.js"></script>
+    <script src="../js/pages/tables/jquery-datatable.js"></script>
+    <script src="../js/pages/ui/dialogs.js"></script>
+    <script src="../js/demo.js"></script>
 </body>
 <script>
 
--- SYSVER/VIstas/WorkersP2.php
+++ SYSVER/VIstas/WorkersP2.php
@@ -6,39 +6,39 @@
     <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
     <title>SYSVER | bRANDUX</title>
     <!-- Favicon-->
-    <link rel="icon" href="favicon.ico" type="image/x-icon">
+    <link rel="icon" href="../favicon.ico" type="image/x-icon">
 
     <!-- Google Fonts -->
     <link href="https://fonts.googleapis.com/css?family=Roboto:400,700&subset=latin,cyrillic-ext" rel="stylesheet" type="text/css">
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet" type="text/css">
 
     <!-- Bootstrap Core Css -->
-    <link href="plugins/bootstrap/css/bootstrap.css" rel="stylesheet">
+    <link href="../plugins/bootstrap/css/bootstrap.css" rel="stylesheet">
 
     <!-- Waves Effect Css -->
-    <link href="plugins/node-waves/waves.css" rel="stylesheet" />
+    <link href="../plugins/node-waves/waves.css" rel="stylesheet" />
 
     <!-- Animation Css -->
-    <link href="plugins/animate-css/animate.css" rel="stylesheet" />
+    <link href="../plugins/animate-css/animate.css" rel="stylesheet" />
 
     <!-- Sweet Alert Css -->
-    <link href="plugins/sweetalert/sweetalert.css" rel="stylesheet" />
+    <link href="../plugins/sweetalert/sweetalert.css" rel="stylesheet" />
 
     
     <!-- Bootstrap Material Datetime Picker Css -->
-    <link href="plugins/bootstrap-material-datetimepicker/css/bootstrap-material-datetimepicker.css" rel="stylesheet" />
+    <link href="../plugins/bootstrap-material-datetimepicker/css/bootstrap-material-datetimepicker.css" rel="stylesheet" />
 
     <!-- Wait Me Css -->
-    <link href="plugins/waitme/waitMe.css" rel="stylesheet" />
+    <link href="../plugins/waitme/waitMe.css" rel="stylesheet" />
 
     <!-- Bootstrap Select Css -->
-    <link href="plugins/bootstrap-select/css/bootstrap-select.css" rel="stylesheet" />
+    <link href="../plugins/bootstrap-select/css/bootstrap-select.css" rel="stylesheet" />
     
     <!-- Custom Css -->
-    <link href="css/style.css" rel="stylesheet">
+    <link href="../css/style.css" rel="stylesheet">
 
     <!-- AdminBSB Themes. You can choose a theme from css/themes instead of get all themes -->
-    <link href="css/themes/all-themes.css" rel="stylesheet" />
+    <link href="../css/themes/all-themes.css" rel="stylesheet" />
 </head>
 
 <body class="theme-red">
@@ -98,7 +98,7 @@
             <!-- User Info -->
             <div class="user-info">
                 <div class="image">
-                    <img src="images/user.png" width="48" height="48" alt="User" />
+                    <img src="../images/user.png" width="48" height="48" alt="User" />
                 </div>
                 <div class="info-container">
                     <div class="name" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">John Doe</div>
@@ -276,44 +276,44 @@
 
     
     <!-- Jquery Core Js -->
-    <script src="plugins/jquery/jquery.min.js"></script>
+    <script src="../plugins/jquery/jquery.min.js"></script>
 
     <!-- Bootstrap Core Js -->
-    <script src="plugins/bootstrap/js/bootstrap.js"></script>
+    <script src="../plugins/bootstrap/js/bootstrap.js"></script>
 
     <!-- Select Plugin Js -->
-    <script src="plugins/bootstrap-select/js/bootstrap-select.js"></script>
+    <script src="../plugins/bootstrap-select/js/bootstrap-select.js"></script>
 
     <!-- Slimscroll Plugin Js -->
-    <script src="plugins/jquery-slimscroll/jquery.slimscroll.js"></script>
+    <script src="../plugins/jquery-slimscroll/jquery.slimscroll.js"></script>
 
     <!-- Jquery Validation Plugin Css -->
-    <script src="plugins/jquery-validation/jquery.validate.js"></script>
+    <script src="../plugins/jquery-validation/jquery.validate.js"></script>
 
     <!-- JQuery Steps Plugin Js -->
-    <script src="plugins/jquery-steps/jquery.steps.js"></script>
+    <script src="../plugins/jquery-steps/jquery.steps.js"></script>
 
     <!-- Sweet Alert Plugin Js -->
-    <script src="plugins/sweetalert/sweetalert.min.js"></script>
+    <script src="../plugins/sweetalert/sweetalert.min.js"></script>
 
     <!-- Waves Effect Plugin Js -->
-    <script src="plugins/node-waves/waves.js"></script>
+    <script src="../plugins/node-waves/waves.js"></script>
 
     <!-- Autosize Plugin Js -->
-    <script src="plugins/autosize/autosize.js"></script>
+    <script src="../plugins/autosize/autosize.js"></script>
 
     <!-- Moment Plugin Js -->
-    <script src="plugins/momentjs/moment.js"></script>
+    <script src="../plugins/momentjs/moment.js"></script>
 
     <!-- Bootstrap Material Datetime Picker Plugin Js -->
-    <script src="plugins/bootstrap-material-datetimepicker/js/bootstrap-material-datetimepicker.js"></script>
+    <script src="../plugins/bootstrap-material-datetimepicker/js/bootstrap-material-datetimepicker.js"></script>
     
     <!-- Custom Js -->
-    <script src="js/admin.js"></script>
-    <script src="js/pages/forms/form-wizard.js"></script>
-    <script src="js/pages/forms/basic-form-elements.js"></script>
+    <script src="../js/admin.js"></script>
+    <script src="../js/pages/forms/form-wizard.js"></script>
+    <script src="../js/pages/forms/basic-form-elements.js"></script>
     <!-- Demo Js -->
-    <script src="js/demo.js"></script>
+    <script src="../js/demo.js"></script>
 </body>
 <script>
 
