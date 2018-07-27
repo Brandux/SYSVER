3e1f1ba52919b6ab5fd0bd737d78119afe829e3e
Brandux <branduxjuarez@upeu.edu.pe>
2018-07-27 18:19:00 -0500

    actak

--- SYSVER/VIstas/ProyectosP1.php
+++ SYSVER/VIstas/ProyectosP1.php
@@ -51,7 +51,70 @@
                                             </button>
                                             <br><br>
                                             <div class="table-responsive" id="tablaProyecto">
-                                                <!-- Aqui va la tabla proyecto-->
+                                            <table class="table table-bordered table-striped table-hover dataTable js-exportable">
+                                                    <thead>
+                                                        <tr>
+                                                            <th>Cliente </th>
+                                                            <th>Proyecto</th>
+                                                            <th>Fecha de duracion</th>
+                                                            <th>Costo total</th>
+                                                            <th>Est de suelos</th>
+                                                            <th>Categoria</th>
+                                                            <th>Estado</th>
+                                                            <th>OPIONES</th>
+                                                        </tr>
+                                                    </thead>
+                                                    <tfoot>
+                                                        <tr>
+                                                            <th>Cliente </th>
+                                                            <th>Proyecto</th>
+                                                            <th>Fecha de duracion</th>
+                                                            <th>Costo total</th>
+                                                            <th>Est de suelos</th>
+                                                            <th>Categoria</th>
+                                                            <th>Estado</th>
+                                                            <th>OPIONES</th>
+                                                        </tr>
+                                                    </tfoot>
+                                                    <tbody>
+                                                          
+                                                        <tr>
+                                                            <td>Michael Bruce</td>
+                                                            <td>Javascript Developer</td>
+                                                            <td>Singapore</td>
+                                                            <td>29</td>
+                                                            <td>2011/06/27</td>
+                                                            <td>2011/06/27</td>
+                                                            <td>2011/06/27</td>
+                                                            <td>
+                                                                <button type="button"  data-toggle="modal" data-target="#edit"  class="btn btn-info btn-circle waves-effect waves-circle waves-float">
+                                                                    <i class="material-icons">edit</i>
+                                                                </button>
+                                                                 <button type="button"  onclick="eliminar()" class="btn btn-danger btn-circle waves-effect waves-circle waves-float">
+                                                                    <i class="material-icons">close</i>
+                                                                </button>
+                                                            </td>
+                                                        </tr>
+                                                        <tr>
+                                                            <td>Donna Snider</td>
+                                                            <td>Customer Support</td>
+                                                            <td>New York</td>
+                                                            <td>27</td>
+                                                            <td>2011/01/25</td>
+                                                            <td>$112,000</td>
+                                                            <td>2011/06/27</td>
+                                                            <td>
+                                                                <button type="button"  data-toggle="modal" data-target="#edit" class="btn btn-info btn-circle waves-effect waves-circle waves-float">
+                                                                    <i class="material-icons">edit</i>
+                                                                </button>
+                                                                 <button type="button"  onclick="eliminar()" class="btn btn-danger btn-circle waves-effect waves-circle waves-float">
+                                                                    <i class="material-icons">close</i>
+                                                                </button>
+                                                            
+                                                            </td>
+                                                        </tr>
+                                                    </tbody>
+                                                </table>
                                             </div>
                                         </div>
                                     </div>
@@ -106,7 +169,6 @@
 </body>
 <script>
     $(document).ready(function(){
-        $('#tablaProyecto').load('Proyecto/tablaProyecto.php');
     });
 
     $("#add").click(function(){
